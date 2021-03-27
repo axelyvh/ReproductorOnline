cfc5fc68dc0832eafceeeab37fcaaee0f8e6151b
Axel Yoliston Vasquez Herrera <axelvasquezherrera@outlook.com>
2021-03-26 22:02:35 -0500

    reproductor ok

---
+++ src/js/script.js
@@ -0,0 +1,16 @@
+const BtnReproducirVideo = document.getElementById("btn_repro_video");
+let player = videojs("video_player");
+
+player.hotkeys({
+  volumeStep: 0.1,
+  seekStep: 5
+});
+
+BtnReproducirVideo.addEventListener("click", function () {
+  let url = document.getElementById("txt_url_video").value;
+  if (url.trim().length == 0) {
+    alert("Ingrese una url por favor");
+    return;
+  }
+  player.src({ type: "video/mp4", src: url });
+});
